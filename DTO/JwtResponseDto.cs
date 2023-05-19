0c0f37c2ccd5d54cf7132353c3a2c852cfb30d16
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2023-05-19 01:39:56 +0100

    Add JwtResponseDto and LoginDto for data transfer through API

---
+++ DTO/JwtResponseDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SubsAPI.DTO
+{
+    public class JwtResponseDTO
+    {
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
---
+++ DTO/LoginDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SubsAPI.DTO
+{
+    public class LoginDto
+    {
+        public string ServiceId { get; set; }
+        public string Password { get; set; }
+    }
+}
