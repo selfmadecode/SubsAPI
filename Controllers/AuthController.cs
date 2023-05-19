cadf4dfb08085c473bb8a22ea32989725e595bfc
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2023-05-19 01:45:33 +0100

    Add auth controller with login endpoint

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SubsAPI.DTO;
+using SubsAPI.Helpers;
+using SubsAPI.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +12,21 @@ namespace SubsAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthController : ControllerBase
+    public class AuthController : BaseController
     {
+        private readonly IAuthService _auth;
+
+        public AuthController(IAuthService auth)
+        {
+            _auth = auth;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(BaseResponse<JwtResponseDTO>), 200)]
+        public async Task<IActionResult> Login([FromBody] LoginDto login)
+        {
+            return ReturnResponse(await _auth.Login(login));
+        }
+
     }
 }
