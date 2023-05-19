0efa424a5561940a12e7cdea46c750117f542c17
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2023-05-19 01:40:46 +0100

    Add base response class

---
+++ Helpers/BaseResponse.cs
@@ -0,0 +1,44 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SubsAPI.Helpers
+{
+    public class BaseResponse<T>
+    {
+        public BaseResponse()
+        {
+        }
+
+        public BaseResponse(T data, string responseMessage = null)
+        {
+            this.Data = data;
+            this.Status = RequestExecution.Successful;
+            this.ResponseMessage = responseMessage;
+        }
+
+        public BaseResponse(string error, List<string> errors = null)
+        {
+            this.Status = RequestExecution.Failed;
+            this.ResponseMessage = error;
+            this.Errors = errors;
+        }
+
+        public BaseResponse(T data, string error, List<string> errors, RequestExecution status)
+        {
+            this.Status = status;
+            this.ResponseMessage = error;
+            this.Errors = errors;
+            this.Data = data;
+        }
+
+        public RequestExecution Status { get; set; }
+
+        public T Data { get; set; }
+
+        public string ResponseMessage { get; set; }
+
+        public List<string> Errors { get; set; }
+    }
+}
