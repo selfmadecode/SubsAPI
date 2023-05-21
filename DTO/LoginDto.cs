c43a0a5fb3cfab6e64d7a579b3814e1fb4d211e4
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2023-05-21 13:16:10 +0100

    Updated property name

--- DTO/LoginDto.cs
+++ DTO/LoginDto.cs
@@ -1,13 +1,20 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace SubsAPI.DTO
 {
     public class LoginDto
     {
+        [JsonPropertyName("service_id")]
+        [Required]
         public string ServiceId { get; set; }
+
+        [Required]
         public string Password { get; set; }
     }
 }
--- DTO/SubscribeDto.cs
+++ DTO/SubscribeDto.cs
@@ -4,22 +4,23 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace SubsAPI.DTO
 {
     public class SubscribeDto
     {
-        [JsonProperty("token")]
+        [JsonPropertyName("token")]
         [Required(ErrorMessage = "Token required")]
         [TokenValidation]
         public string Token { get; set; }
 
-        [JsonProperty("service_id")]
+        [JsonPropertyName("service_id")]
         [Required(ErrorMessage = "Service Id required")]
         public string ServiceId { get; set; }
         
-        [JsonProperty("phone_number")]
+        [JsonPropertyName("phone_number")]
         [Required(ErrorMessage = "Phone number required")]
         [Phone]
         public string PhoneNumber { get; set; }
